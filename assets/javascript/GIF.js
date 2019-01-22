333a3cc3a69017c3d5195a12d605470c9e15ca35
William Fehlberg <williamfehlberg@Williams-MacBook-Pro.local>
2019-01-21 23:27:06 -0600

    push1

--- assets/javascript/GIF.js
+++ assets/javascript/GIF.js
@@ -1,2 +1,12 @@
 //create entry and submit button
 //write the values from the submission to create a button with the entered text
+var APIKey = "wBHVSLIThNZhXx17AlZz2hPZ1CC9PaRK";
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=golf&api_key=" + APIKey + "&limit=10";
+
+$.ajax({
+    url: queryURL,
+    method: 'GET'
+}).then(function(response)  {
+    console.log(response);
+});
+
